9da973d9037450e096c50ce451bc1aa96ffc07bc
shreyaskudav <dekudav@gmail.com>
2021-11-16 15:03:51 +0530

    Update streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -11,33 +11,41 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+model = load_model('model_baby_99acc_22-8_4.h5')
+    
 def negate(x):
     return x * -1
 
+def is_video(file):
+    if file.type.split('/')[0] == 'video':
+        return True
+    else:
+        return False
+
 def main():
     st.header("Baby Gesture Detection")
-    st.write("You can find sample media for testing [here]()")
+    st.write("You can find sample media for testing [here](https://github.com/shreyas-jk/Baby-Action-Detection-Safety-System-Prototype/tree/main/streamlit/sample)")
     st.markdown("""---""")
-    image_file = st.file_uploader("Upload Media", type=["png","jpg","jpeg","mp4"])
+    file_object = st.file_uploader("Upload Media", type=["mp4"])
 
-    if image_file is not None:
-
+    if file_object is not None and is_video(file_object):
         class_list = []
-        mp_holistic = mp.solutions.holistic
-        model = load_model(os.path.join('templete', 'model_baby_99acc_22-8_4.h5'))
         sequence = []
     
         # Save file
-        destination = os.path.join("templete", "uploads", image_file.name)
+        destination = os.path.join("uploads", file_object.name)
         with open(destination, "wb") as f:
-            f.write((image_file).getbuffer())
+            f.write((file_object).getbuffer())
 
         cap = cv2.VideoCapture(destination)
         i = 0
+        t = st.empty()
         with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
             while cap.isOpened():
                 i += 1
-                print('Frame {0}'.format(i), end='\r')
+                t.markdown('Please wait.. Processing frame {0}'.format(i))
                 
                 # Read feed
                 ret, frame = cap.read()
@@ -47,7 +55,10 @@ def main():
                     image, results = util.mediapipe_detection(frame, holistic)
                 except:
                     break
-            
+                
+                # Draw landmarks
+                util.draw_styled_landmarks(mp_holistic, mp_drawing, image, results)
+                
                 # Add keypoints
                 keypoints = util.extract_keypoints(results)
                 sequence.append(keypoints)
@@ -69,6 +80,8 @@ def main():
         df = pd.DataFrame({'classes': class_list})
         df = df.groupby('classes')["classes"].size().reset_index(name='count')
         total_count = df['count'].sum()
+        
+        t.empty()
 
         # Print results
         for index, row in df.iterrows():
